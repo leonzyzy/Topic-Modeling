7710d073b88bb594dc9b4f4b0d7ad6be277ade89
Zhiyuan <liz27@miamioh.edu>
2024-02-26 16:13:24 -0500

    Update topic_modeling.py

--- topic_modeling.py
+++ topic_modeling.py
@@ -1,14 +1,18 @@
 import pandas as pd
+import numpy as np
 import re
 import string
 import nltk
 import hdbscan
-from umap import UMAP
+import umap.umap_ as umap
 from sentence_transformers import SentenceTransformer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from kneed import KneeLocator
 
 # Stopword removal, converting uppercase into lower case, and lemmatization
 nltk.download('punkt')
@@ -19,31 +23,72 @@ nltk.download('wordnet')
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
+# define model
+model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+
 # import data
-df = pd.read_csv('Reviews.csv', encoding = "ISO-8859-1")
+df = pd.read_csv('tripadvisor_hotel_reviews.csv')
 
-# load model
-sbert = SentenceTransformer('all-MiniLM-L6-v2')
+# random sample of 1000 reviews
+df = np.array(df.sample(50)['Review']).tolist()
 
-# sample some rows
-text = df['Text'][:100]
-embedding = sbert.encode(text, convert_to_numpy=True, show_progress_bar=True)
+# define a function to optimize the number of clusters
+def optimal_k(inputs, max_k):
+    # define the range of clusters
+    intra_cluster_distances = []
+    inter_cluster_distances = []
+    optimal_dist = []
+    
+    for k in range(2, max_k + 1):
+        kmeans = KMeans(n_clusters=k, random_state=42)
+        kmeans.fit(inputs)
+        
+        # Inertia is the sum of squared distances of samples to their closest cluster center
+        intra_cluster_distances.append(kmeans.inertia_)
+        
+        # Calculate inter-cluster distance using silhouette score
+        silhouette_avg = silhouette_score(inputs, kmeans.labels_)
+        inter_cluster_distances.append(silhouette_avg)
+        
+        # we want to max inter_cluster_distances and min intra_cluster_distances
+        # equals to max inter_cluster_distances - min intra_cluster_distances
+        # we want to find the point where the difference is maximized
+        optimal_dist.append(inter_cluster_distances[-1] - intra_cluster_distances[-1])
+    
+    # use kneedle algorithm to find the optimal number of clusters
+    kn = KneeLocator(range(2, 10 + 1), 
+                     optimal_dist, 
+                     curve='concave', 
+                     direction='increasing')
 
-# perform umap
-map = UMAP(n_components=8, init='random', random_state=528)
-embedding_proj = map.fit_transform(embedding)
+    return kn.knee
 
-# use dbscan
-cluster = hdbscan.HDBSCAN(min_cluster_size=5).fit(embedding_proj)
-cluster_labels = cluster.fit_predict(embedding_proj)
+# define a function to assign labels to clusters
+def assign_labels(x, model, max_k):
+    # embedding
+    model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+    embeddings = model.encode(x, convert_to_numpy=True, show_progress_bar=True)
+    
+    # perform UMAP
+    map = umap.UMAP(n_components=5, n_neighbors=12, random_state=528)
+    embeddings_proj = map.fit_transform(embeddings)
+    
+    # find the optimal number of clusters
+    k = optimal_k(embeddings_proj, max_k)
+    kmeans = KMeans(n_clusters=k, random_state=42).fit(embeddings_proj)
+    y = kmeans.fit_predict(embeddings_proj)
+    text_group = pd.DataFrame({'text': x, 'label': y})
+    
+    return text_group
 
-text_group = pd.DataFrame(text)
-text_group['label'] = cluster_labels
-
+text_group = assign_labels(df, model, 10)
+    
+    
+# find topic for each cluster using tfidf
 # for each label perform tfidf
 for i in range(0, len(text_group['label'].unique())):
     label = text_group[text_group['label'] == i]
-    label_text = label['Text']
+    label_text = label['text']
     
     # Preprocess text data
     preprocessed_texts = []
@@ -64,5 +109,5 @@ for i in range(0, len(text_group['label'].unique())):
     tfidf_matrix.columns = ['response'+ str(i) for i in range(1, len(tfidf_matrix.columns)+1)]
     tfidf_matrix['count'] = tfidf_matrix.sum(axis=1)
     tfidf_matrix = tfidf_matrix.sort_values(by ='count', ascending=False)[:10] 
-    print(tfidf_matrix['count'].head(10))
-
+    print(tfidf_matrix['count'].head(5))
+    
