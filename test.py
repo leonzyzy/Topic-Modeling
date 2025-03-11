f605a29e92feae30825950480540eccb40652b80
Zhiyuan Li <liz27@miamioh.edu>
2025-03-11 13:41:14 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ data = torch.tensor([[1, 2, 3],
                      [4, 5, 6],  
                      [7, 8, 9]], dtype=torch.float)
 
-# Define the max number of rows (50 in this case)
-max_rows = 50
+# Define the max number of rows
+max_rows = 3
 
 # Initialize a list to store the transformed rows
 expanded_data = []
@@ -15,20 +15,16 @@ expanded_data = []
 for i in range(1, data.shape[0] + 1):
     cumulative_rows = data[:i]
     
-    # If the number of rows exceeds max_rows, truncate it
-    if cumulative_rows.shape[0] > max_rows:
-        cumulative_rows = cumulative_rows[:max_rows]
-    
     # If the number of rows is less than max_rows, pad it
     padding_size = max_rows - cumulative_rows.shape[0]
     padded_rows = torch.cat([cumulative_rows, torch.zeros(padding_size, data.shape[1])], dim=0)
     
-    # Add each individual transaction to expanded_data
-    for j in range(padded_rows.shape[0]):
-        expanded_data.append(padded_rows[j])
+    # Add the padded rows as a separate entry in expanded_data
+    expanded_data.append(padded_rows)
 
 # Convert list to a tensor
 expanded_data = torch.stack(expanded_data)
 
 # Output the transformed tensor
-print(expanded_data)
+for t in expanded_data:
+    print(t)
