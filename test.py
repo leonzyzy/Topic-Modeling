0b9e65206abd0af78e49102606fa6ada19fcc887
Zhiyuan Li <liz27@miamioh.edu>
2025-05-05 18:16:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 
+Spearheaded the implementation of the first stream training pipeline using LitData and PyTorchJob, enabling efficient large-scale transformer training on massive datasets.
+
+Achieved a 100x acceleration in training speed through optimization of the pipeline, significantly enhancing productivity.
+
+Delivered transformer benchmark results using the full training dataset, setting the foundation for subsequent model improvements.
+
+Onboarded and mentored Plaksha and Karthik on LitData and PyTorchJob, empowering the team to conduct multiple experiments and ablation studies, exploring diverse transformer architectures and parameter tuning (including sequence length).
+
+Led the team to achieve the best benchmark result of 0.130, outperforming the production GBM model, showcasing significant improvements.
+
+Innovated and implemented cutting-edge model architectures, including seq2seq and skip-connection, driving a new best benchmark result (0.133).
+
+Contributed high-quality code, model libraries, and unit tests to C1-Bumblebee, elevating the overall framework’s capabilities.
+
+Played a key role in delivering results-focused insights through clear and compelling slide decks for business partners and stakeholders in the fraud space.
